a80729d2e7b2e31d9e84210ce785b5d7456c978f
John Boyle <john@Unknown.domain>
2016-05-17 21:14:52 -0700

    Print all app windows with titles containing argv[2]

--- GetWindowID.m
+++ GetWindowID.m
@@ -6,6 +6,6 @@ int main(int argc, char **argv)
 	NSArray *windows = (NSArray *)CGWindowListCopyWindowInfo(kCGWindowListExcludeDesktopElements,kCGNullWindowID);
 	for(NSDictionary *window in windows)
 		if ([[window objectForKey:(NSString *)kCGWindowOwnerName] isEqualToString:[NSString stringWithUTF8String:argv[1]]])
-			if ([[window objectForKey:(NSString *)kCGWindowName] isEqualToString:[NSString stringWithUTF8String:argv[2]]])
-				printf("%d\n", [[window objectForKey:(NSString *)kCGWindowNumber] intValue]);
+			if ([[window objectForKey:(NSString *)kCGWindowName] containsString:[NSString stringWithUTF8String:argv[2]]])
+				printf("Title: %s, ID: %d\n", [[window objectForKey:(NSString *)kCGWindowName] UTF8String], [[window objectForKey:(NSString *)kCGWindowNumber] intValue]);
 }
